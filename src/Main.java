1211788af59baf5fb2a026b285cbd929d911ea28
dali546 <35352237+dali546@users.noreply.github.com>
2019-02-25 14:13:38 +0000

    first program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
+import java.sql.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i;
+
+        for (i = 1; i <= 300; i++) {
+            boolean Fizz = false;
+            boolean Buzz = false;
+            boolean Bang = false;
+            boolean Bong = false;
+            boolean Fezz = false;
+            boolean reverse = false;
+
+            if (i % 3 == 0) {
+                Fizz = true;
+            }
+            if (i % 5 == 0) {
+                Buzz = true;
+            }
+            if (i % 7 == 0) {
+                Bang = true;
+            }
+            if (i % 11 == 0) {
+                Bong = true;
+            }
+            if (i % 13 == 0) {
+                Fezz = true;
+            }
+            if (i % 17 == 0){
+                reverse = true;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            if (Fizz) {
+                stringBuilder.append("Fizz");
+            }
+            if (Buzz) {
+                stringBuilder.append("Buzz");
+            }
+            if (Bang) {
+                stringBuilder.append("Bang");
+            }
+            if (Bong) {
+                stringBuilder.replace(0, stringBuilder.length(), "Bong");
+            }
+            if (Fezz) {
+                stringBuilder.insert(
+                        (stringBuilder.indexOf("B") == -1 ? 0 : stringBuilder.indexOf("B")),
+                        "Fezz"
+                );
+            }
+            if (reverse) {
+                String[] arr = stringBuilder.toString().split("(?<=\\G...)");
+                stringBuilder.replace(0,stringBuilder.length(), arr[]);
+            }
+            System.out.println(stringBuilder);
+            System.out.println(i);
+        }
     }
 }
