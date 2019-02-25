52481b4ce27ec53b5194ab83ee23cc3dfcfd3af6
dali546 <35352237+dali546@users.noreply.github.com>
2019-02-25 16:56:58 +0000

    final

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,80 @@
-import java.io.IOException;
-import java.sql.Array;
+import java.io.StringReader;
 import java.util.*;
 
 public class Main {
 
+    /**
+     * FizzBuzz Challenge... The Bane of New Programmers Existence
+     *
+     * @param args - Arguments this Program can take
+     *             args[0] - Pick from 3,5,7,11,13,17 - seperate with ','
+     */
     public static void main(String[] args) {
-        int i;
-        for (i = 1; i <= 300; i++) {
-            boolean Fizz, Buzz, Bang, Bong, Fezz, reverse;
-            Fizz = Buzz = Bang = Bong = Fezz = reverse = false;
+        List<String> stringList = new ArrayList<>();
+        stringList = Arrays.asList(args[0].split(","));
+        System.out.println("You requested the following options: " + stringList.toString());
 
-            if (i % 3 == 0) {
-                Fizz = true;
-            }
-            if (i % 5 == 0) {
-                Buzz = true;
-            }
-            if (i % 7 == 0) {
-                Bang = true;
-            }
-            if (i % 11 == 0) {
-                Bong = true;
-            }
-            if (i % 13 == 0) {
-                Fezz = true;
-            }
-            if (i % 17 == 0) {
-                reverse = true;
-            }
-            StringBuilder stringBuilder = new StringBuilder();
-            List<String> strings = new ArrayList<>();
-            if (Fizz) {
-                strings.add("Fizz");
-            }
-            if (Fezz) {
-                strings.add("Fezz");
-            }
-            if (Buzz) {
-                strings.add("Buzz");
-            }
-            if (Bang) {
-                strings.add("Bang");
-            }
-            if (Bong) {
-                strings.clear();
-                strings.add("Bong");
-            }
-            if (reverse) {
-                Collections.reverse(strings);
-            }
-
-            for (String str : strings) {
-                stringBuilder.append(str);
-            }
-            if (!strings.isEmpty()) {
-                System.out.println(stringBuilder.toString() + ": " + i);
-            } else {
-                System.out.println("This number is Dumb: " + i);
-            }
-        }
-    }
-}
-
-class CountToMax {
-
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ok You want me to count upto a number." +
+        System.out.println("Ok You want me to count upto a number." +
                 "What number do you want me to count to?");
-        try{
-            int i = scanner.nextInt();
+
+        try {
+            int k = scanner.nextInt();
             System.out.println("Ok *breaths*");
-            for (int j=0;j<=i;j++) {
-                System.out.println(j);
+            for (int i = 1; i <= k; i++) {
+                boolean Fizz, Buzz, Bang, Bong, Fezz, reverse;
+                Fizz = Buzz = Bang = Bong = Fezz = reverse = false;
+
+                if (i % 3 == 0 && stringList.contains("3")) {
+                    Fizz = true;
+                }
+                if (i % 5 == 0 && stringList.contains("5")) {
+                    Buzz = true;
+                }
+                if (i % 7 == 0 && stringList.contains("7")) {
+                    Bang = true;
+                }
+                if (i % 11 == 0 && stringList.contains("11")) {
+                    Bong = true;
+                }
+                if (i % 13 == 0 && stringList.contains("13")) {
+                    Fezz = true;
+                }
+                if (i % 17 == 0 && stringList.contains("17")) {
+                    reverse = true;
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                List<String> strings = new ArrayList<>();
+                if (Fizz) {
+                    strings.add("Fizz");
+                }
+                if (Fezz) {
+                    strings.add("Fezz");
+                }
+                if (Buzz) {
+                    strings.add("Buzz");
+                }
+                if (Bang) {
+                    strings.add("Bang");
+                }
+                if (Bong) {
+                    strings.clear();
+                    strings.add("Bong");
+                }
+                if (reverse) {
+                    Collections.reverse(strings);
+                }
+
+                for (String str : strings) {
+                    stringBuilder.append(str);
+                }
+                if (!strings.isEmpty()) {
+                    System.out.println(stringBuilder.toString() + ": " + i);
+                } else {
+                    System.out.println("This number is Dumb: " + i);
+                }
             }
-        } catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("Use some common sense. Enter a NUMBER");
         }
     }
